f9e26d91e34ab49f95b9147e05bbd9cd7908c5b7
Yamkela <owamjames1@gmail.com>
2017-11-06 18:09:46 +0200

    adding filter

--- public/api.js
+++ public/api.js
@@ -1,15 +1,13 @@
  $("document").ready(function() {
    console.log('success');
-   var dropTemp = document.querySelector("#dropTemp").innerHTML;
-   var dropDownTemplateInst = Handlebars.compile(dropTemp);
    var Output = document.querySelector('#Output');
    var data = document.querySelector('.data')
-  //  Output.innerHTML = dropDownTemplateInst({
-  //       key: shoeData
-  //   });
-
    var tableTemplate = document.querySelector("#tableTemplate").innerHTML;
    var tableTemplateInst = Handlebars.compile(tableTemplate);
+   var sizeFilter = document.querySelector('#sizeFilter')
+   var brandFilter = document.querySelector('#brandFilter')
+   var sizeButton = document.querySelector('#sizeButton')
+   var brandButton = document.querySelector('#brandButton')
 
 $.ajax({
 type: 'GET',
@@ -22,5 +20,70 @@ success:function(shoesData){
 }
 
 })
+    var new_color = document.getElementById("new_color");
+    var new_size = document.getElementById("new_size");
+    var new_brand = document.getElementById("new_brand");
+    var new_price = document.getElementById("new_price");
+    var in_stock = document.getElementById("in_stock");
+  addBtn.addEventListener('click', function() {
 
- })
+
+            var size = new_size.value;
+            var stock = in_stock.value;
+          var color=  new_color.value;
+            var brand= new_brand.value;
+            var price = new_price.value
+
+var newData ={
+  size:size,
+  brand:brand,
+  price:price,
+  color:color,
+  in_stock: stock
+
+}
+$.ajax({
+type: 'POST',
+url: '/api/shoes',
+datatype: 'json',
+data: newData,
+success:function(shoesData){
+  console.log(shoesData);
+  data.innerHTML = tableTemplateInst({shoes:shoesData})
+
+}
+
+})
+
+    })
+    sizeButton.addEventListener('click', function() {
+  var sizes = sizeFilter.value;
+      $.ajax({
+      type: 'GET',
+      url: '/api/shoes/size/'+ sizes,
+      datatype: 'json',
+      success:function(shoesData){
+        console.log(shoesData);
+        data.innerHTML = tableTemplateInst({shoes:shoesData})
+
+      }
+
+      })
+
+    })
+    brandButton.addEventListener('click', function() {
+  var brands = brandFilter.value;
+      $.ajax({
+      type: 'GET',
+      url: '/api/shoes/brand/'+ brands,
+      datatype: 'json',
+      success:function(shoesData){
+        console.log(shoesData);
+        data.innerHTML = tableTemplateInst({shoes:shoesData})
+
+      }
+
+      })
+
+})
+  });
